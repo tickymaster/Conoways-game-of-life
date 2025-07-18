7023577fd3925f587e0b47e2480d85b1be3b6f92
tickymaster <lolonix123@gmail.com>
2025-07-18 20:41:23 +0200

    Added custom a x b grid option

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -4,20 +4,24 @@
 #include <unistd.h>
 
 
-void gameOfLife(size_t grid_size = 10);
-std::vector<std::vector<bool>> generateGrid(size_t grid_size = 10);
+void gameOfLife(size_t grid_size_rows = 10, size_t grid_size_cols = 10);
+std::vector<std::vector<bool>> generateGrid(size_t grid_size_rows = 10, size_t grid_size_cols = 10);
 size_t getNumberOfNeighbors(int row, int col, const std::vector<std::vector<bool>>& grid);
 void printGrid(const std::vector<std::vector<bool>>& grid);
 
 int main(){
-    size_t size;
-    std::cout << "Welcome to the game of life!\n" << "How large do you want the grid to be (nxn): " << std::endl;
-    std::cin >> size;
-    gameOfLife(size);
+    size_t grid_size_rows;
+    size_t grid_size_cols;
+    std::cout << "Welcome to the game of life!\n" << "How large do you want the grid to be (axb): " << std::endl;
+    std::cout << "a: ";
+    std::cin >> grid_size_rows;
+    std::cout << "b: ";
+    std::cin >> grid_size_cols;
+    gameOfLife(grid_size_rows, grid_size_cols);
 }
 
-std::vector<std::vector<bool>> generateGrid(size_t grid_size){
-    std::vector<std::vector<bool>> grid(grid_size, std::vector<bool>(grid_size, false));
+std::vector<std::vector<bool>> generateGrid(size_t grid_size_rows, size_t grid_size_cols){
+    std::vector<std::vector<bool>> grid(grid_size_rows, std::vector<bool>(grid_size_cols, false));
     
     std::cout << "Welcome to the grid generation tool!\n\n" 
     << "Specify the locations to put a live cell into: \n" 
@@ -37,7 +41,7 @@ std::vector<std::vector<bool>> generateGrid(size_t grid_size){
         if (col == -1 && row == -1)
         {
             break;
-        } else if((col < 0 || col >= grid_size) || (row < 0 || row >= grid_size))
+        } else if((col < 0 || col >= grid_size_cols) || (row < 0 || row >= grid_size_rows))
         {
             std::cerr << "Error: Invaid index, please reenter valid indicies...\n";
         } else
@@ -46,14 +50,14 @@ std::vector<std::vector<bool>> generateGrid(size_t grid_size){
             std::cout << "Added live cell at coordinates (" << row << ',' << col << "), awaiting new coordinate input..\n";
         }
     }
-    std::cout << "Returning a grid of size = (" << grid_size << ',' << grid_size << ')' << std::endl;
+    std::cout << "Returning a grid of size = (" << grid_size_rows << ',' << grid_size_cols << ')' << std::endl;
     return grid; 
     
 }
 
-void gameOfLife(size_t grid_size){
-    std::vector<std::vector<bool>> old_grid = generateGrid(grid_size);
-    std::vector<std::vector<bool>> new_grid(grid_size, std::vector<bool>(grid_size, false));
+void gameOfLife(size_t grid_size_rows, size_t grid_size_cols){
+    std::vector<std::vector<bool>> old_grid = generateGrid(grid_size_rows, grid_size_cols);
+    std::vector<std::vector<bool>> new_grid(grid_size_rows, std::vector<bool>(grid_size_cols, false));
 
     /*
     List of rules:
@@ -72,9 +76,9 @@ void gameOfLife(size_t grid_size){
 
     while (true)
     {
-        for (size_t row = 0; row < grid_size; row++)
+        for (size_t row = 0; row < grid_size_rows; row++)
         {
-            for (size_t col = 0; col < grid_size; col++)
+            for (size_t col = 0; col < grid_size_cols; col++)
             {
                 size_t number_of_neighbors = getNumberOfNeighbors(row, col, old_grid);
 
